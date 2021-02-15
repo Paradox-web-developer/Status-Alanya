46792f53156c7644a2ebe9342fcc6465b44978ce
Paradox-web-developer <60786601+Paradox-web-developer@users.noreply.github.com>
2021-02-15 19:26:23 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-/*let menuButton = document.querySelector('.menu-button');
-let menu = document.querySelector('.menu');
-menuButton.addEventListener('click', function() {
-	menuButton.classList.toggle('menu-button-active');
-	menu.classList.toggle('header-active');
 
-})
-*/
 let modal = document.querySelector("#myModal");
 let closeButton = document.querySelector("#close-button");
 let openButton = document.querySelector("#btn_modal_window");
